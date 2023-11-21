b63020839e3414d3a634da519aaac1063ce7722d
MedHam94 <hammas.m.mh@gmail.com>
2023-11-21 22:15:46 +0100

    stoped in the ejs

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mainRoutes = require('./server/routes/main')
 const app = express()
+const connectDB = require('./server/config/db');
 const PORT = process.env.PORT || 5000
 
+connectDB()
 app.use(express.static('public'))
 
 // Templating engine
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
 app.set('layout', 'layouts/main');
+
+
 
 app.use('/', mainRoutes)
 
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const router = express()
+const Post = require('../models/Post')
+// Get / 
+// Home
+router.get('', async (req, res) => {
+  const locals = {
+    title: "NodeJs Blog",
+    description: "Simple Blog created with NodeJs, Express & MongoDb."
+  }
 
+  try {
+    const data = await Post.find();
+    res.render('index', { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
 
-router.get('/', (req, res)=> {
-    res.render('index')
-})
+});
 
+function insertPostData () {
+      Post.insertMany([
+        {
+          title: "Building APIs with Node.js",
+          body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js"
+        },])
+
+    }
+    insertPostData()
 router.get('/about', (req,res)=>{
     res.render('about')
 })
